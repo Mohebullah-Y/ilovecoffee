4917199edaa7835137e984a346827a551266415a
Mohebullah Yousofi <mohebullah.yousofi@gmail.com>
2025-07-30 12:34:31 +0430

    change status code by decorator and express

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('coffees')
 export class CoffeesController {
     @Get()
-    findAll(){
-        return 'This action returns all coffees';
+    findAll(@Res() response){
+        response.status(210).send('This action returns all coffees');
     }
 
     @Get(':id')
@@ -13,6 +14,7 @@ export class CoffeesController {
     }
 
     @Post()
+    @HttpCode(HttpStatus.GONE)
     create(@Body() body){
         return body;
     }
