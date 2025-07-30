a80d964dc9bb38878c5e37255dcaffcefd1c416d
Mohebullah Yousofi <mohebullah.yousofi@gmail.com>
2025-07-30 16:23:17 +0430

    add pagination using Query decorator

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
 import { response } from 'express';
 
 @Controller('coffees')
 export class CoffeesController {
     @Get()
-    findAll(response){
-        return 'This action returns all coffees';
+    findAll(@Query() paginationQuery){
+        const {limit, offset} = paginationQuery;
+        return `This action returns all coffees. Limit: ${limit}, Offset: ${offset}`;
     }
 
     @Get(':id')
