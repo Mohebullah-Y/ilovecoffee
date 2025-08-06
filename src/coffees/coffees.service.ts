c20888f549c26abefa23bb0f07f0c51588e7305c
Mohebullah Yousofi <mohebullah.yousofi@gmail.com>
2025-08-06 16:16:36 +0430

    add pagination and use it to findAll method

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Flavor } from './entities/flavor.entity';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 
 //Service saperate business logic from the controller
 //business logic saperation makes the logic reusable in muliple parts of application
@@ -21,9 +22,12 @@ export class CoffeesService {
       private readonly flavorRepository: Repository<Flavor>
     ){}
 
-    findAll(){
+    findAll(paginationQuery: PaginationQueryDto){
+      const {limit, offset} = paginationQuery;
       return this.coffeeRepository.find({
-         relations: ['flavors']
+         relations: ['flavors'],
+         skip: offset,
+         take: limit
       });
     }
 
