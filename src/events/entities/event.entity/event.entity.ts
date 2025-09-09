fc1757290cf81a528e1492a8e21eeae727a8608b
Mohebullah Yousofi <mohebullah.yousofi@gmail.com>
2025-09-09 14:17:09 +0430

    add index to event schema

--- src/events/entities/event.entity/event.entity.ts
+++ src/events/entities/event.entity/event.entity.ts
@@ -2,11 +2,11 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import * as mongoose from "mongoose";
 
 @Schema()
-export class Event {
+export class Event extends mongoose.Document {
     @Prop()
     type: string;
 
-    @Prop()
+    @Prop({ index: true })
     name: string;
 
     @Prop(mongoose.SchemaTypes.Mixed)
@@ -14,3 +14,4 @@ export class Event {
 }
 
 export const EventSchema = SchemaFactory.createForClass(Event);
+EventSchema.index({ name: 1, type: -1 });
